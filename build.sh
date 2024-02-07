95a056066f65efd46b5eb2f2cb4a4ad808b136f3
Olexander Bryl <edlrian814@gmail.com>
2024-02-07 17:11:02 +0200

    Try adding access to media files

--- build.sh
+++ build.sh
@@ -7,3 +7,12 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+mkdir -p /vol/web/media
+
+adduser --disabled-password --no-create-home django-user
+
+chown -R django-user:django-user /vol/
+chmod -R 755 /vol/web/
+chgrp -R www-data /vol/web/
+chmod -R g+w  /vol/web/
