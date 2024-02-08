5e3fce76e55bd2a6e6196b40816e8ef97ede268f
Olexander Bryl <edlrian814@gmail.com>
2024-02-08 16:25:12 +0200

    Play with celery some more

--- build.sh
+++ build.sh
@@ -7,3 +7,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+celery -A cherwood_shop worker -l info --detach
+
+celery -A cherwood_shop beat -l info --detach
--- start.sh
+++ start.sh
@@ -3,7 +3,3 @@
 set -o errexit
 
 gunicorn cherwood_shop.wsgi:application
-
-celery -A cherwood_shop worker -l info
-
-celery -A cherwood_shop beat -l info
