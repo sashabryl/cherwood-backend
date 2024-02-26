6fa846c684073f7b6c5eb93bec25d0ed19b6247d
Olexander Bryl <edlrian814@gmail.com>
2024-02-26 17:14:47 +0200

    Make ACCESS_TOKEN_LIFETIME 10 min & REFRESH_TOKEN_LIFETIME 7 days

--- cherwood_shop/settings.py
+++ cherwood_shop/settings.py
@@ -195,8 +195,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=15),
-    "REFRESH_TOKEN_LIFETIME": timedelta(seconds=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
 
 SPECTACULAR_SETTINGS = {
